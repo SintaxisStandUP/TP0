4d825979a86e511a5305806e6e310c1381fd5585
Francisco Cortez <franciscoandrescortez@gmail.com>
2018-06-20 20:03:52 -0300

    nuevoscanner

---
+++ scanner nuevo.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <ctype.h>
+
+//Puede recibir un estado y el caracter leido y realiza una determinada acción.
+int automata (int estado, int carac) {
+
+return estado;
+}
