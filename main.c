e225247d1a398a6b07f859db0b5bcc97108f9a66
Francisco Cortez <franciscoandrescortez@gmail.com>
2018-06-24 16:41:58 -0300

    terminado

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*
 TP 0 – 2018 - Un escáner elemental
-Grupo 02
+Grupo 02 - 2da Entrega
 
 Cortez Francisco Andres	1638130
 Kuric Mariela		1627740
@@ -13,7 +13,7 @@ Tabla del automata:
 2+	99	99	99	99
 3	3	3	10	4
 4+	99	99	99	99
-10	3	1	99	0
+10	3	1	10	0
 
 Referencias:
 0-  ->	Estado inicial
@@ -22,7 +22,6 @@ Referencias:
 3   ->	Reconociendo identificador
 4+  ->	Identificador reconocido
 10  ->	Error
-Nota: En la implementación del código, con fines practicos se agrego un estado "5".
 */
 
 #include <stdio.h>
@@ -31,58 +30,33 @@ Nota: En la implementación del código, con fines practicos se agrego un estado
 
 int main()
 {
-int estado = 0;
-int estadoAnt = 0;
-int carac;
+int token = 0;
 int acum_ident = 0;
 int acum_const = 0;
 int acum_error = 0;
-int resultados[20];
-int i = 0;
-int boolAux = 0;
-
 FILE *archivo;
-archivo = fopen ("lexemas.txt","r");
-
-while (!feof(archivo))
+archivo = fopen("lexemas.txt","r");
+int i = 0;
+while (token != EOF) //Hasta fin de cadena.
 {
-	carac = getc(archivo); //Lee un caracter
-	estado = automata (estado, carac);//Invoco al scanner que me devuelve la categoria lexica(estado)
-	if (estado == 4 || estado == 2 || estado == 10)
-	{
-	if (estado == 10 && boolAux)
-		boolAux = 0;
-	if (estado != 10 && boolAux)
-	{
-		resultados[i] = 10; //Marca el error que hubo antes.
-		i++;			//Para el caso #&/$823
-		boolAux = 0;
-	}
+	token=scanner(&archivo);
 
-	resultados[i] = estado;
-	i++;
-	estado = 0;
-	}
-	if(estado == 5  && !boolAux)
+	if(token == 2)
 	{
-	boolAux = 1;
+		printf ("constante entera\n");
+		acum_const+=1;
 	}
-}
-
-	for (int h=0; h<i ;h++)
-    {
-		if (resultados[h]==2){
-			printf ("constante entera\n");
-			acum_const+=1;}
-		else{
-			if (resultados[h]==4)
-			{	printf ("identificador\n");
-				acum_ident+=1;}
-					else
-					{	printf ("error\n");
-						acum_error+=1; }
-			}
-}	
+	else if(token == 4)
+	{
+		printf ("identificador\n");
+		acum_ident+=1;
+	}
+	else if (token == 10)
+	{
+		printf ("error\n");
+		acum_error+=1;
+	}
+}			
 	printf ("----\n");
 	printf ("Totales: \n");
 	printf ("Identificadores %d \n",acum_ident);
--- scanner.c
+++ scanner.c
@@ -1,60 +1,96 @@
 #include <stdio.h>
 #include <ctype.h>
+#include "scanner.h"
 
-//Puede recibir un estado y el caracter leido y realiza una determinada acción.
-int automata (int estado, int carac) {
-switch (estado)
+int scanner (FILE **archivo) {
+//FILE *archivo;
+int carac;
+
+//archivo = fopen("lexemas.txt","r");
+int TT[6][4] = {{3, 1, 10, 0},
+		{10, 1, 10, 2},		
+		{99, 99, 99, 99},
+		{3, 3, 10, 4},
+		{99, 99, 99, 99},
+		{3, 1, 10, 0}};
+int estado = 0;
+while (estado != 2 && estado != 4 && estado != 10)
+{
+	carac = getc(*archivo); //Leer con getc
+	if (carac == EOF)
+	{
+		return EOF;
+	}
+	switch (estado)
 	{
 	case 0:
 	if (isspace(carac)){
-		estado=0;
+		estado = TT[0][3];
 		}
 	else if (isdigit(carac)){
-		estado = 1;
+		estado = TT[0][1];
 		}
 	else if (isalpha(carac)){
-		estado = 3;
+		estado = TT[0][0];
 		}
 	else {
-		estado = 5;
+		estado = TT[0][2];
 		}
-	
 	break;
+	
 	case 1:
 	if (isdigit(carac)){
-		estado=1;
+		estado=TT[1][1];
 		}
 	else if (isspace(carac)){
-		estado = 2;
+		estado = TT[1][3];
 		}
+	else 
+		estado = TT[1][0];
+		
 	break;
 
 	case 3:
 	if (isspace(carac)){
-		estado=4;
+		estado=TT[3][3];
 		}
 	else if (isdigit(carac) || isalpha(carac)){
-		estado = 3;
+		estado = TT[3][1];
 		}
-	break;
-
-	case 5:
-	if (isspace(carac)){
-		estado=10;
-		}
-	else if (isdigit(carac)){
-		estado = 1;
-		}
-	else if (isalpha(carac)){
-		estado = 3;
-		}
-
-	else {
-		estado = 5;
-		}
+	else estado = TT[3][2];
 	break;
 	}
-return estado;
+
+} //end While
+
+while(1)
+{
+	if(estado == 2) //estado aceptor
+	{
+		return 2; //retorna token cte	
+	}
+	else if(estado == 4) // estado aceptor
+	{
+		return 4; //retorna token identificador
+	}
+	else if (estado==10) // estado de error
+	{
+		if (isspace(carac)){
+			estado = TT[5][3];
+			return 10; // retorna token error
+			}
+		else if (isdigit(carac) || isalpha(carac)){
+			estado = TT[5][1]; //Finaliza el error (cte/ide)
+			carac = ungetc(carac, *archivo); //retrocedo un caracter
+			return 10;
+			}
+		else { //Sigue el error
+			estado = TT[5][2];
+			carac = getc(*archivo);			
+			}
+	}
+}
+
 }
 /*
 Tabla del automata:
@@ -64,21 +100,5 @@ Tabla del automata:
 2+	99	99	99	99
 3	3	3	10	4
 4+	99	99	99	99
-10	3	1	99	0
+10	3	1	10	0
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
