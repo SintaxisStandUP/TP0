09a483359e60c92a13a3cc1c578cad6d8b00b1af
Francisco Cortez <franciscoandrescortez@gmail.com>
2018-06-28 18:14:12 -0300

    espero

--- scanner.c
+++ scanner.c
@@ -6,74 +6,63 @@ static const int TT[8][5] = {{3, 1, 5, 0, 7},
 			{99, 99, 99, 99, 99},
 			{3, 3, 4, 4, 4},
 			{99, 99, 99, 99, 99},
-			{6, 6, 5, 6, 6}, //nueva columna recono error
+			{6, 6, 5, 6, 6},
 			{99, 99, 99, 99, 99},
 			{99, 99, 99, 99, 99}};
-	int estado = INICIAL;
 
 int identificarCaracter (int carac);
 
 int scanner (FILE **archivo) 
 {
-	int carac;
-	int caracter; //es un auxiliar para no modificar carac y poder hacer "ungetc(); con lo que esta guardado en carac"
-
-	
+int estado = INICIAL;
+int carac;
+int caracter;
 
 	while (estado != CTE_REC && estado != IDE_REC && estado != ERROR && estado != FDT)
 	{
 		carac = getc(*archivo); //Leer con getc
-		if (carac == EOF) //Identificamos el caracter
-		{
-			caracter = END;
-		}
-
-		else 
-		{
-			int identificarCaracter (int carac);
-		}
+		caracter = identificarCaracter(carac);	
 		estado = TT[estado][caracter];
 	}
-//tenemos en "carac" guardado un simbolo que es centinela, por lo tanto hacemos ungetc();
-		if(estado == CTE_REC) //estado aceptor
-		{
+
+	switch(estado)
+	{
+		case CTE_REC:
 			carac = ungetc(carac, *archivo);
-			return TOKEN_CTE;	
-		}
-		else if(estado == IDE_REC) // estado aceptor
-		{
+			return TOKEN_CTE;
+			break;	
+		case IDE_REC:
 			carac = ungetc(carac, *archivo);
 			return TOKEN_IDE;
-		}
-		else if (estado == ERROR) // estado de error
-		{
+			break;
+		case ERROR:
 			carac = ungetc(carac, *archivo);
 			return TOKEN_ERROR;
-		}
-		else if(estado == FDT) // estado aceptor
-		{
+			break;
+		case FDT:
 			return TOKEN_FDT;
-		}
+	}
 }
-
 int identificarCaracter (int carac)
 {
-	if (isspace(carac))
+	if (carac == EOF)
+	{
+		return END;
+	}
+	else if (isspace(carac))
 	{	
 		return ESPACIO;
 	}
+	else if (isdigit(carac))
+	{
+		return DIGITO;
+	}
+	else if (isalpha(carac))
+	{
+		return LETRA;
+	}
 	else 
-		if {isdigit(carac))
-		{
-			return DIGITO;
-		}
-		else
-			if (isalpha(carac))
-			{
-				return LETRA;
-			}
-			else 
-			{
-				return OTRO;
-			}
+	{
+		return OTRO;
+	}
 }	
